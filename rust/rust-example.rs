339410d1055673dae18b49a5d64b4c61aba8b026
gudi <gudi>
2015-03-22 17:03:13 +0800

    change

---
+++ rust/rust-example.rs
@@ -0,0 +1,15 @@
+// This code is editable and runnable!
+fn main() {
+    // A simple integer calculator:
+    // `+` or `-` means add or subtract by 1
+    // `*` or `/` means multiply or divide by 2
+
+    let program = "+ + * - /";
+    let mut accumulator = 0;
+
+    for token in program.chars() {
+        match token {
+            '+' 
+        }
+    }
+}
