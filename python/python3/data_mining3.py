67ae78e760721bd60281c21a3f846af8ee4ff89f
gudi <huolingdfsq@gmail.com>
2015-04-07 13:10:28 +0800

    change

--- python/python3/data_mining3.py
+++ python/python3/data_mining3.py
@@ -1,1 +1,7 @@
-__author__ = 'gudi'
+import random
+
+def buckets(filename, bucketName, separator, classColumn):
+    """the original data is in the file named filename
+    bucketName is the prefix for all the bucket names
+    separator is the character that divides the columns
+    
