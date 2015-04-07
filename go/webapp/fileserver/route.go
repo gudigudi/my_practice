443268bd0ed1260955b8545bb327aa9a955af4ff
gudi <huolingdfsq@gmail.com>
2015-04-07 13:10:12 +0800

    change

--- go/webapp/fileserver/route.go
+++ go/webapp/fileserver/route.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-    "net/http"
-    "github.com/russross/blackfriday"
+	"github.com/russross/blackfriday"
+	"net/http"
+	"os"
 )
 
 func main() {
-    http.H
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
 }
