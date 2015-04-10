240a5a007cd23a5590fc711b606955513a0c42a7
gudi <huolingdfsq@gmail.com>
2015-04-10 17:48:36 +0800

    Math.toDegrees() in java

---
+++ java/java_tutorial/ToDegrees.java
@@ -0,0 +1,9 @@
+public class ToDegrees {
+    public static void main(String[] args) {
+        double x = 45.0;
+        double y = 30.0;
+
+        System.out.println(Math.toDegrees(x));
+        System.out.println(Math.toDegrees(y));
+    }
+}
