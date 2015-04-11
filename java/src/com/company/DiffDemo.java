94758e69c42d53bc4a3b504249efa58e58cece82
gudi <huolingdfsq@gmail.com>
2015-04-11 20:54:36 +0800

    say good night!

--- java/src/com/company/DiffDemo.java
+++ java/src/com/company/DiffDemo.java
@@ -1,7 +1,22 @@
 package com.company;
 
+import java.util.Date;
+
 /**
  * Created by gudi on 4/11/15.
  */
 public class DiffDemo {
+    public static void main(String[] args) {
+        try {
+            long start = System.currentTimeMillis();
+            System.out.println(new Date() + "\n");
+            Thread.sleep(5 * 60 * 10);
+            System.out.println(new Date() + "\n");
+            long end = System.currentTimeMillis();
+            long diff = end - start;
+            System.out.println("Difference is : " + diff);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
