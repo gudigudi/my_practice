257e36311bc92e0c5afaf7d34ab46ec99c756f42
gudi <huolingdfsq@gmail.com>
2015-04-16 09:55:55 +0800

    multithreading in java

--- java/src/com/company/Multithreading/DisplayMessage.java
+++ java/src/com/company/Multithreading/DisplayMessage.java
@@ -3,5 +3,18 @@ package com.company.Multithreading;
 /**
  * Created by gudi on 4/16/15.
  */
-public class DisplayMessage {
+public class DisplayMessage implements Runnable {
+
+    private String message;
+
+    public DisplayMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(message);
+        }
+    }
 }
