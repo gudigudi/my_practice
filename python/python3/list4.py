83f381a8aba810908ae3cf1603e2f3b1c9ff13b9
gudi <huolingdfsq@gmail.com>
2015-04-18 09:57:35 +0800

    list test

---
+++ python/python3/list4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+table = {1975: 'Holy Grail',
+         1979: 'Life of Brian',
+         1983: 'The Meaning of Life'}
+
+print(table[1975])
+print(list(table.items()))
