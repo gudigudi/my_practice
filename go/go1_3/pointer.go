012fea7c37328438f361f9e29bffe0857f3d5a82
gudi <huolingdfsq@gmail.com>
2015-04-21 11:11:30 +0800

    change pointer.go: fmt.Println ot fmt.Printf

--- go/go1_3/pointer.go
+++ go/go1_3/pointer.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	var i1 = 5
-	fmt.Println("An integer: %d, its location in memory: %p\n", i1, &i1)
+	fmt.Printf("An integer: %d, its location in memory: %p\n", i1, &i1)
 	var intP *int
 	intP = &i1
-	fmt.Println("The value at memory location %p is %d", intP, *intP)
+	fmt.Printf("The value at memory location %p is %d", intP, *intP)
 }
