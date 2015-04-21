3111fc0865697e0f56c14f47bec48fc60a308c53
gudi <huolingdfsq@gmail.com>
2015-04-21 10:00:56 +0800

    change in angular

--- angular/angular-phonecat/app/js/controllers.js
+++ angular/angular-phonecat/app/js/controllers.js
@@ -1,9 +1,8 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope){
+function PhoneListCtrl($scope){
   $scope.phones = [
     {'name': 'Nexus S',
      'snippet': 'Fast just got faster with Nexus S.'},
@@ -12,4 +11,4 @@ phonecatApp.controller('PhoneListCtrl', function($scope){
     {'name': 'MOTOROLA XOOM',
      'snippet': 'The Next, Next Generation tablet.'}
   ];
-});
+}
--- angular/angular-phonecat/test/unit/controllersSpec.js
+++ angular/angular-phonecat/test/unit/controllersSpec.js
@@ -9,3 +9,16 @@ describe('controllers', function() {
   });
 
 });
+
+describe('PhoneCat controllers', function(){
+
+  describe('PhoneListCtrl', function(){
+
+    it('should create "phone" model with 3 phones', function(){
+      var scope = {};
+      ctrl = new PhoneListCtrl(scope);
+
+      expect(scope.phones.length).toBe (3);
+    });
+  });
+});
