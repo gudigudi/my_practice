1813e4cb8594678b046a2e9a26b3ec6806453bd4
gudi <huolingdfsq@gmail.com>
2015-04-26 14:41:50 +0800

    move the old Duck code to here Duck with class

---
+++ design_pattern/head_first_design_pattern/Duck_class/Duck.java
@@ -0,0 +1,10 @@
+public abstract class Duck {
+
+    public void quack() {
+        System.out.println("Duck can quack");
+    }
+    public void swim() {
+        System.out.println("Duck can swim");
+    }
+    public abstract void display();
+}
