af3e48b7a38682e476a62ec2c6511eaf2825fb5e
gudi <huolingdfsq@gmail.com>
2015-04-27 08:10:05 +0800

    Subject and Observer

---
+++ design_pattern/head_first_design_pattern/Observer_pattern/ConcreteSubject.java
@@ -0,0 +1,17 @@
+public class ConcreteSubject implements Subject {
+
+    public void registerObserver() {
+    }
+
+    public void removeObserver() {
+    }
+
+    public void notifyObserver() {
+    }
+
+    public void getState() {
+    }
+
+    public void setState() {
+    }
+}
---
+++ design_pattern/head_first_design_pattern/Observer_pattern/Subject.java
@@ -0,0 +1,7 @@
+interface Subject {
+
+    public void registerObserver();
+    public void removeObserver();
+    public void notifyObserver();
+
+}
