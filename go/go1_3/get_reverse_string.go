ef8d9a78423a9be544ff3fbf194e167654e759f7
gudi <huolingdfsq@gmail.com>
2015-05-05 19:02:54 +0800

    change

--- go/go1_3/get_reverse_string.go
+++ go/go1_3/get_reverse_string.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	input := "I am a student"
 
-	var l []string
+	output := strings.Join(strings.Split(input, " "), " ")
 
-	index := 0
-	for i := 0; i < len(input); i++ {
-		if input[i] != ' ' {
-			append(l[index], input[i])
-		}
-	}
-
-	for v := range l {
-		fmt.Println(v)
-	}
+	fmt.Println(output)
 }
