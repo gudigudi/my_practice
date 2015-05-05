bbba8f7b452be0b387f68be4c4b164c6fddc8bcb
gudi <huolingdfsq@gmail.com>
2015-05-05 15:31:29 +0800

    switch in golang is great and useful

---
+++ go/go1_3/switch_input.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter your name : ")
+	input, err := inputReader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("There were errors reading, exiting program.")
+		return
+	}
+
+	fmt.Printf("Your name is %s", input)
+
+	switch input {
+	case "Philop\r\n":
+		fmt.Println("Welcome Philop!")
+	case "Chris\r\n":
+		fmt.Println("Welcome Chris!")
+	case "Ivo\r\n":
+		fmt.Println("Welcome Ivo!")
+	default:
+		fmt.Printf("You are not welcome here! Goodbye!")
+	}
+}
