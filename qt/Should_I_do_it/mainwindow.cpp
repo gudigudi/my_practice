11cebac588bc4f61c04f7039f77cbbec279a2422
gudi <huolingdfsq@gmail.com>
2015-05-16 13:19:29 +0800

    a simple qt5 project

--- qt/Should_I_do_it/mainwindow.cpp
+++ qt/Should_I_do_it/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QRadioButton *btn1 = new QRadioButton("hello", this);
+    QWidgetWindow widgetWindow = new QWidgetWindow();
+    widgetWindow.show();
 }
