e884e919b07f5b007acf6cc93d76f1fe171383db
gudi <huolingdfsq@gmail.com>
2015-05-17 13:19:37 +0800

    Command Pattern in Java

---
+++ design_pattern/src/head_first_design_pattern/Command/Command.java
@@ -0,0 +1,9 @@
+package head_first_design_pattern.Command;
+
+/**
+ * Created by gudi on 5/17/15.
+ */
+public interface Command {
+
+    public void execute();
+}
---
+++ design_pattern/src/head_first_design_pattern/Command/Light.java
@@ -0,0 +1,15 @@
+package head_first_design_pattern.Command;
+
+/**
+ * Created by gudi on 5/17/15.
+ */
+public class Light {
+
+    public void on() {
+        System.out.println("Light on!");
+    }
+
+    public void off() {
+        System.out.println("Light off");
+    }
+}
