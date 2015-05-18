d2cb3fdce80a1d49572ffe049d2fe8f343b50ff9
gudi <huolingdfsq@gmail.com>
2015-05-18 21:07:39 +0800

    change

--- qt/design_pattern/design_pattern.pro
+++ qt/design_pattern/design_pattern.pro
@@ -12,9 +12,8 @@ TARGET = design_pattern
 TEMPLATE = app
 
 
-SOURCES += main.cpp\
-        mainwindow.cpp
+SOURCES += main.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  +=
 
 FORMS    += mainwindow.ui
--- qt/design_pattern/main.cpp
+++ qt/design_pattern/main.cpp
@@ -1,11 +1,24 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QString>
+#include <QLabel>
+#include <QWidget>
+#include <QDebug>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+    QApplication myapp(argc, argv);
+    QWidget wid;
+    qDebug() << "sizeof widget: " << sizeof(wid)
+             << " sizeof qapplication: " << sizeof(myapp);
+    QString message;
+    QTextStream buf(&message);
+    buf << "A QWidget is " << sizeof(wid)
+        << " bytes. \nA QObject is " << sizeof(QObject)
+        << " bytes. \nA OApplication is " << sizeof(myapp)
+        << " bytes.";
+    qDebug() << message;
+    QLabel label(message);
+    label.show();
+    return myapp.exec();
 }
