47ec51c0fcc124a2becd39ff7b724daede51a391
gudi <huolingdfsq@gmail.com>
2015-05-21 11:06:57 +0800

    a simple opgnGL program

---
+++ openGL/a_simple_openGL_program.c
@@ -0,0 +1,22 @@
+#include <GL/gl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(-0.5, 0.5);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
+
+    sleep(10);
+    return 0;
+}
