63a9d0273bc30772695864a3fac8190d8bfd6140
gudi <huolingdfsq@gmail.com>
2015-05-26 23:27:03 +0800

    js ex

---
+++ java/src/com/company/PackageExample/Client.java
@@ -0,0 +1,7 @@
+package com.company.PackageExample;
+
+/**
+ * Created by gudi on 5/26/15.
+ */
+public class Client {
+}
---
+++ js/jsex.js
@@ -0,0 +1,14 @@
+var mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
+
+var luigisDebt = 140;
+luigisDebt -= 35;
+console.log(luigisDebt);
+
+var one = 1, two = 2;
+console.log(one + two);
+
+console.log(Math.max(2, 4));
