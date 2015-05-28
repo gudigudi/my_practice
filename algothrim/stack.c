9bd74f1d984b21efa6a0b0a084a6487693fab4fd
gudi <huolingdfsq@gmail.com>
2015-05-28 08:17:10 +0800

    stack for DFS

---
+++ algothrim/stack.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * define Stack
+ * struct: item[1024], length
+ * func: push, pop
+ */
+
+typedef struct _Stack {
+    char item[1024];
+    unsigned int length;
+} Stack;
+
+void push(char c)
+{
+
+}
+
