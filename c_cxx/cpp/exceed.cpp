feb4906d2a1550cbd9a94bf48a011e0a57f6f814
gudi <huolingdfsq@gmail.com>
2015-05-29 18:40:53 +0800

    overflow the numbers

---
+++ c_cxx/cpp/exceed.cpp
@@ -0,0 +1,25 @@
+// exceed.cpp -- exceeding some integer limits
+#include <iostream>
+#define ZERO 0
+#include <climits>
+
+int main()
+{
+    using namespace std;
+    short sam = SHRT_MAX;
+    unsigned short sue = sam;
+
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Add $1 to each account." << endl << "Now ";
+    sam++;
+    sue++;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited.\nPoor Sam!" << endl;
+    sam = ZERO;
+    sue = ZERO;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl << "Lucky Sue!" << endl;
+
+	return 0;
+}
