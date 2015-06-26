1745d6c0a6e9ae7118c361bb52a0f437d1c28571
gudi <huolingdfsq@gmail.com>
2015-06-26 18:51:39 +0800

    learn jquery as a dash

---
+++ js/jquery_as_a_dash/hello.js
@@ -0,0 +1,15 @@
+// alert("Hello JavaScript, your new best friend!");
+//
+// var makeSandwich = function() {
+//   alert("Get bread");
+//   alert("Spread the butter");
+//   alert("Spread the jam");
+//   alert("Cut off the crusts");
+//   alert("Put in a little bag");
+// };
+//
+// makeSandwich();
+
+$(function() {
+  $("body").html("Hello, jquery");
+});
