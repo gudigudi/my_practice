8d4d867f20b8d5ceba0f99382362f9d0aa9b94b4
gudi <huolingdfsq@gmail.com>
2015-10-19 10:07:06 +0800

    build a simple C project with Makefile

---
+++ android/build-system/test1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "utility.h"
+
+int main(int argc, char *argv[])
+{
+  printf("Hello, getNumber=%d\n", getNumber());
+  return 0;
+}
---
+++ android/build-system/test1/utility.c
@@ -0,0 +1,6 @@
+#include "utility.h"
+
+int getNumber()
+{
+  return 2;
+}
