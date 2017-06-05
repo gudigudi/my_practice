8e542f3a3743bc701ea5172acd42b810a44f85bf
gudi <huolingdfsq@gmail.com>
2017-06-05 11:18:16 +0800

    just clean

--- Frontend/html5/js/Object.js
+++ Frontend/html5/js/Object.js
@@ -1,22 +1,12 @@
-false.toString();  // 'false'
-[1, 2, 3].toString();  // '1, 2, 3'
-
-function Foo() {}
-Foo.bar = 1;
-console.log(Foo.bar);  // 1
-
-var obj = {
-    bar: 1,
-    foo: 2,
-    baz: 3
+var o = {
+  "1p": "Hello World",
+  "h w": "Hello World",
+  "p+q": "Hello World"
 };
 
-obj.bar = undefined;
-obj.foo = null;
-delete obj.baz;
+var p = {
+  p: function(x) {return 2*x;}
+};
 
-for (var i in obj) {
-    if (obj.hasOwnProperty(i)) {
-        console.log(i, ' ' + obj[i]);
-    }
-}
+console.log(o.p(1))
+
