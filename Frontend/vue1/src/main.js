8dc47222932cf618192e6e8d1af82c66e4a7effd
Jiade Dai <huolingdfsq@gmail.com>
2017-08-08 23:30:40 +0800

    add vue-router; add vuex support

--- Frontend/vue1/src/main.js
+++ Frontend/vue1/src/main.js
@@ -1,15 +1,76 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
-// import router from './router'
+import router from './router'
 
 Vue.config.productionTip = false
+
+// Vue.use(Vuex)
+
+// defined modules
+const ModuleA = {
+  state: {
+    count: 0
+  },
+  getters: {
+    countFilterA (state, getters, rootState) {
+      if (state.count > 5) {
+        return state.count
+      }
+    }
+  },
+  mutations: {
+    increateA (state) {
+      state.count++
+    }
+  },
+  actions: {
+    increateAsyncA ({state, commit, rootState}) {
+      if ((state.count + rootState.count) % 2 === 1) {
+        commit('increateA')
+      }
+    }
+  }
+}
+
+const ModuleB = {
+  state: {
+    count: 0
+  },
+  getters: {
+    countFilterB (state, getters, rootState) {
+      if (state.count > 5) {
+        return state.count + rootState.count
+      }
+    }
+  },
+  mutations: {
+    increateB (state) {
+      state.count++
+    }
+  }
+}
+
+// use modules
+const store = new Vuex.Store({
+  state: {
+    count: 10
+  },
+  modules: {
+    a: ModuleA,
+    b: ModuleB
+  }
+})
+
+// Vue.use(plugins, {someOption: true})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
+  store,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
