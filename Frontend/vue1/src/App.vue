300bdff03662b9f87746071f3338a4b47f8984cb
Jiade Dai <huolingdfsq@gmail.com>
2017-08-08 23:31:34 +0800

    update App.vue: add vuex support && others

--- Frontend/vue1/src/App.vue
+++ Frontend/vue1/src/App.vue
@@ -1,20 +1,78 @@
 <template>
   <div id="app">
-    <layouts></layouts>
+
+    <!--<file-upload></file-upload>-->
+    <!--<loading size="massive" message="Loading..."></loading>-->
+    <!--<layouts></layouts>-->
     <!--<router-view></router-view>-->
+    <input v-model="data"/>
+    <p>{{data}}</p>
+    {{countFilterA}}
+    {{countFilterB}}
+    <p>module-a:
+      <button @click="increateA">{{a.count}}</button>
+    </p>
+    <p>module-b:
+      <button @click="increateB">{{b.count}}</button>
+    </p>
+    <button @click="increateAsyncA">actions</button>
+
+    <select-search></select-search>
+
+    <app-footer></app-footer>
   </div>
 </template>
 <script>
   import Layouts from './layouts/layouts'
+  import FileUpload from './components/FileUpload'
+  import Loading from './components/Loading'
+  import AppFooter from './AppFooter'
+
+  import example from './mixins/example'
+
+  import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
+  import SelectSearch from './components/selectSearch.vue'
+
   export default {
     name: 'app',
     components: {
+      SelectSearch,
+      AppFooter,
+      Loading,
+      FileUpload,
       Layouts
     },
-    data () {
-      return {}
+    computed: {
+      ...mapState([
+        'a',
+        'b'
+      ]),
+      ...mapGetters([
+        'countFilterA',
+        'countFilterB'
+      ])
     },
-    methods: {}
+    data () {
+      return {
+        data: 'lala'
+      }
+    },
+    methods: {
+      ...mapMutations([
+        'increateA',
+        'increateB'
+      ]),
+      ...mapActions([
+        'increateAsyncA'
+      ]),
+      logic () {
+        console.log('do the logic about hello page')
+      }
+    },
+    mixins: [example],
+    mounted () {
+      this.logic()
+    }
   }
 </script>
 <style lang="scss">
