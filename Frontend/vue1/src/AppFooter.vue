a7bf5ca83abc6b062f5f1c25575ffebde72e141c
Jiade Dai <huolingdfsq@gmail.com>
2017-08-08 23:32:55 +0800

    add AppFooter.vue

---
+++ Frontend/vue1/src/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app-footer">
+  </div>
+</template>
+<script>
+  export default {
+    name: 'app-footer',
+    data () {
+      return {
+        a: 'a'
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  #app-footer {
+
+  }
+</style>
