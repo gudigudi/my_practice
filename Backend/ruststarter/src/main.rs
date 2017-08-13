5dd86bf0e6b0607bd008a017926be47ce2c02090
Jiade Dai <huolingdfsq@gmail.com>
2017-08-13 23:01:04 +0800

    update main.rs: add static variables

--- Backend/ruststarter/src/main.rs
+++ Backend/ruststarter/src/main.rs
@@ -1,7 +1,10 @@
 extern crate rand;
 
-use std::io;
+//use std::io;
 use rand::Rng;
+
+static MAX_HEALTH: i32=100;
+static GAME_NAME: &'static str = "Monster Attack";
 
 fn main(){
     println!("Hello,world!");
