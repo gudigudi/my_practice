2c0b0d83ebdb1b61083210ec8258187efca93b84
Jiade Dai <huolingdfsq@gmail.com>
2017-08-13 23:03:11 +0800

    add component: DatePicker.vue

---
+++ Frontend/vue1/src/components/DatePicker.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+<script>
+  export default {
+    name: 'date-picker',
+    props: {
+      value: {
+        validator: function (val) {
+          return val === null || val instanceof Date || typeof value === 'string'
+        }
+      },
+      name: {
+        value: String
+      },
+    }
+  }
+</script>
+<style>
+
+</style>
