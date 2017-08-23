bd2c3994d40bf7da13adc75ee9257366e450be1d
Jiade Dai <huolingdfsq@gmail.com>
2017-08-24 00:07:40 +0800

    update: component cal-panel: add template & script

--- Frontend/vue1/src/components/event-calendar/Cal-Panel.vue
+++ Frontend/vue1/src/components/event-calendar/Cal-Panel.vue
@@ -1,11 +1,44 @@
 <template>
-
+  <div class="cal-wrapper">
+    <div class="cal-header">
+      <div class="l" @click="preMonth">
+        <div class="arrow-left icon">&nbsp</div>
+      </div>
+      <div class="title">{{curYearMonth}}</div>
+      <div class="r" @click="nextMonth">
+        <div class="arrow-right icon">&nbsp</div>
+      </div>
+    </div>
+    <div class="cal-body">
+      <div class="weeks">
+        <span v-for="dayName in dayNames" class="item">{{dayName}}</span>
+      </div>
+      <div class="dates">
+        <div v-for="date in dayList" class="item" :class="{
+          today: date.status ? (today==date.date) : false,
+          event: date.status ? (date.title!=undefined) : false,
+          [calender.options.className] : (date.date == selectedDay)
+        }">
+          <p class="date-num" @click="handleChangeCurday(date)"
+             :style="{color: date.title !=undefined ? ((date.date==selectedDay) ? '#fff' : customColor):'inherit'}">
+            {{date.statuc ? date.date.split('/')[2] : '&nbsp'}}
+          </p>
+          <span v-if="date.status ? (today === date.date) : false" class="is-today"
+                :style="{backgruondColor:customColor}"></span>
+          <span v-if="date.status ? (date.title !== undefined) : false" class="is-event"
+                :style="{borderColor:customColor,backgroundColor:(date.date=selectedDay) ? customColor :'inherit'}"></span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   export default {
     name: 'cal-panel',
     date () {
-      return {}
+      return {
+        dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+      }
     },
     props: {
       events: {
@@ -50,7 +83,27 @@
       },
       today () {
         let dateObj = new Date()
-        return `${}`
+        return `${dateObj.getFullYear()}/${dateObj.getMonth() + 1}/${dateObj.getDate()}`
+      },
+      curYearMonth () {
+        let tempData = Date.parse(new Date(`${this.calendar.params.curYear}/${this.calendar.params.curMonth + 1}/01`))
+        return this.vm.$moment(data, 'dd/MM/yyyy')
+      },
+      customColor () {
+        return this.calendar.options.color
+      }
+    },
+    methods: {
+      nextMonth () {
+        this.$EventCalendar.nextMonth()
+        this.$emit('month-changed', this.curYearMonth)
+      },
+      preMonth () {
+        this.$EventCalendar.preMonth()
+        this.$emit('month-changed', this.curYearMonth)
+      },
+      handleChangeCurday (date) {
+        this.$emit('cur-day-changed', date.date)
       }
     }
   }
