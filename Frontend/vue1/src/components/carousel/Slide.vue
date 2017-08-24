9c32a2fcde0ff71b7708c26b3bc8e767084cb3b3
Jiade Dai <huolingdfsq@gmail.com>
2017-08-25 00:07:24 +0800

    add: sub-component of carousel:navigation/pagination/slide

---
+++ Frontend/vue1/src/components/carousel/Slide.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="carousel-slide">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'slide',
+    data () {
+      return {
+        width: null
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .carousel-slide {
+    flex-basis: inherit;
+    flex-grow: 0;
+    flex-shrink: 0;
+    user-select: none;
+  }
+</style>
