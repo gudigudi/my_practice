4b22a144723d125ba640bdb58c6be3b3ddbfb831
Jiade Dai <huolingdfsq@gmail.com>
2017-08-30 21:01:05 +0800

    update App.vue: make App as PWA

--- Frontend/vue1/src/App.vue
+++ Frontend/vue1/src/App.vue
@@ -1,78 +1,20 @@
 <template>
   <div id="app">
-
-    <!--<file-upload></file-upload>-->
-    <!--<loading size="massive" message="Loading..."></loading>-->
-    <!--<layouts></layouts>-->
-    <!--<router-view></router-view>-->
-    <input v-model="data"/>
-    <p>{{data}}</p>
-    {{countFilterA}}
-    {{countFilterB}}
-    <p>module-a:
-      <button @click="increateA">{{a.count}}</button>
-    </p>
-    <p>module-b:
-      <button @click="increateB">{{b.count}}</button>
-    </p>
-    <button @click="increateAsyncA">actions</button>
-    <select-search></select-search>
-    <tree-view></tree-view>
-    <app-footer></app-footer>
+    <router-view></router-view>
   </div>
 </template>
 <script>
-  import Layouts from './layouts/layouts'
-  import FileUpload from './components/FileUpload'
-  import Loading from './components/Loading'
-  import AppFooter from './AppFooter'
-
-  import example from './mixins/example'
-
-  import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
-  import SelectSearch from './components/selectSearch.vue'
-  import TreeView from './components/treeview/TreeView.vue'
-
   export default {
     name: 'app',
-    components: {
-      TreeView,
-      SelectSearch,
-      AppFooter,
-      Loading,
-      FileUpload,
-      Layouts
-    },
-    computed: {
-      ...mapState([
-        'a',
-        'b'
-      ]),
-      ...mapGetters([
-        'countFilterA',
-        'countFilterB'
-      ])
-    },
     data () {
       return {
         data: 'lala'
       }
     },
     methods: {
-      ...mapMutations([
-        'increateA',
-        'increateB'
-      ]),
-      ...mapActions([
-        'increateAsyncA'
-      ]),
       logic () {
         console.log('do the logic about hello page')
       }
-    },
-    mixins: [example],
-    mounted () {
-      this.logic()
     }
   }
 </script>
