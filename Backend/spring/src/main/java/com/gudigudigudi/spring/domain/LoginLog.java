e373922f3d49b0e4e6faec327f16de0a143927c6
Jiade Dai <huolingdfsq@gmail.com>
2017-09-13 22:46:50 +0800

    add: model: loginlog

--- Backend/spring/src/main/java/com/gudigudigudi/spring/domain/LoginLog.java
+++ Backend/spring/src/main/java/com/gudigudigudi/spring/domain/LoginLog.java
@@ -10,4 +10,35 @@ public class LoginLog implements Serializable {
     private Date loginDate;
 
 
+    public int getLoginLogId() {
+        return loginLogId;
+    }
+
+    public void setLoginLogId(int loginLogId) {
+        this.loginLogId = loginLogId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
+    }
 }
