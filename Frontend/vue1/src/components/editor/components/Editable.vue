f1ec0595a28c427af63d4381f501014f900dc3e3
Jiade Dai <huolingdfsq@gmail.com>
2017-09-14 22:58:17 +0800

    beautify

--- Frontend/vue1/src/components/editor/components/Editable.vue
+++ Frontend/vue1/src/components/editor/components/Editable.vue
@@ -121,162 +121,162 @@
         oSel.addRange(oRange)
       },
       fontsize (name, value) {
-        let selection = window.getSelection();
-        let range = this.getRange();
+        let selection = window.getSelection()
+        let range = this.getRange()
         if (!range || range.collapsed) {
-          return;
+          return
         }
-        let childNodes = range.cloneContents().childNodes;
+        let childNodes = range.cloneContents().childNodes
         if (childNodes.length === 1 && childNodes[0].nodeType === 1 && childNodes[0].tagName.toLowerCase() === 'span') {
-          let span = range.extractContents().childNodes[0];
-          span.style.fontSize = value;
-          range.insertNode(span);
-          range.selectNode(span);
-          selection.removeAllRanges();
-          selection.addRange(range);
+          let span = range.extractContents().childNodes[0]
+          span.style.fontSize = value
+          range.insertNode(span)
+          range.selectNode(span)
+          selection.removeAllRanges()
+          selection.addRange(range)
         } else {
           if (navigator.userAgent.indexOf('Chrome') !== -1 && navigator.userAgent.indexOf('Edge') === -1) {
             if (document.queryCommandSupported('styleWithCss')) {
-              document.execCommand('styleWithCss', false, true);
+              document.execCommand('styleWithCss', false, true)
             }
-            document.execCommand('fontSize', false, 7);
-            let container = range.commonAncestorContainer;
-            container.nodeType === 3 && (container = container.parentNode);
-            container.tagName.toLowerCase() === 'span' && (container = container.parentNode);
+            document.execCommand('fontSize', false, 7)
+            let container = range.commonAncestorContainer
+            container.nodeType === 3 && (container = container.parentNode)
+            container.tagName.toLowerCase() === 'span' && (container = container.parentNode)
             Array.prototype.forEach.call(container.getElementsByTagName('span'), function (span) {
               if (span.style.fontSize.trim() === '-webkit-xxx-large' || span.style.fontSize.trim() === 'xx-large') {
-                span.style.fontSize = value;
+                span.style.fontSize = value
               }
-              span.normalize();
-            });
+              span.normalize()
+            })
           } else {
             if (document.queryCommandSupported('styleWithCss')) {
-              document.execCommand('styleWithCss', false, false);
+              document.execCommand('styleWithCss', false, false)
             }
-            document.execCommand('fontSize', false, 7);
+            document.execCommand('fontSize', false, 7)
 
-            let fontList = [], spanList = [];
-            let container = range.commonAncestorContainer;
-            container.nodeType === 3 && (container = container.parentNode);
-            container.tagName.toLowerCase() === 'font' && (container = container.parentNode);
-            fontList = container.getElementsByTagName('font');
+            let fontList = [], spanList = []
+            let container = range.commonAncestorContainer
+            container.nodeType === 3 && (container = container.parentNode)
+            container.tagName.toLowerCase() === 'font' && (container = container.parentNode)
+            fontList = container.getElementsByTagName('font')
 
             for (let i = 0; i < fontList.length; i++) {
-              let font = fontList[i];
-              let span = document.createElement('span');
+              let font = fontList[i]
+              let span = document.createElement('span')
               Array.prototype.forEach.call(font.attributes, function (attr) {
-                attr.nodeName === 'size' ? span.style.fontSize = value : span.setAttribute(attr.nodeName, attr.nodeValue);
-              });
-              span.innerHTML = font.innerHTML;
-              span.querySelectorAll('span').length !== 0 && veUtil.command.format(span, 'span', 'fontSize');
-              span.normalize();
-              font.parentNode.replaceChild(span, font);
-              spanList.push(span);
-              i--;
+                attr.nodeName === 'size' ? span.style.fontSize = value : span.setAttribute(attr.nodeName, attr.nodeValue)
+              })
+              span.innerHTML = font.innerHTML
+              span.querySelectorAll('span').length !== 0 && veUtil.command.format(span, 'span', 'fontSize')
+              span.normalize()
+              font.parentNode.replaceChild(span, font)
+              spanList.push(span)
+              i--
             }
-            range.setStartBefore(spanList[0]);
-            range.setEndAfter(spanList[spanList.length - 1]);
-            selection.removeAllRanges();
-            selection.addRange(range);
+            range.setStartBefore(spanList[0])
+            range.setEndAfter(spanList[spanList.length - 1])
+            selection.removeAllRanges()
+            selection.addRange(range)
           }
         }
       },
       formatBlock (name, value) {
-        let ua = navigator.userAgent.toLowerCase();
+        let ua = navigator.userAgent.toLowerCase()
         if (ua.match(/rv:([\d.]+)\) like gecko/) || ua.match(/msie ([\d.]+)/)) {
-          let range = this.getRange();
+          let range = this.getRange()
           if (!range || range.collapsed) {
-            alert(this.lang.ieMsg);
+            alert(this.lang.ieMsg)
           } else {
-            document.execCommand('formatblock', false, '<' + value.toUpperCase() + '>');
+            document.execCommand('formatblock', false, '<' + value.toUpperCase() + '>')
           }
         } else {
-          document.execCommand('formatblock', false, value);
+          document.execCommand('formatblock', false, value)
         }
       },
 
       formatContent (obj, tagName, cssName) {
-        let temp = [];
-        let pattern = {fontSize: /font\-size:\s?\d+px;/g, verticalAlign: /vertical\-align:\s?(sub|super);/g};
-        let nodeList = obj.getElementsByTagName(tagName);
+        let temp = []
+        let pattern = {fontSize: /font\-size:\s?\d+px;/g, verticalAlign: /vertical\-align:\s?(sub|super);/g}
+        let nodeList = obj.getElementsByTagName(tagName)
         for (let i = 0; i < nodeList.length; i++) {
-          let node = nodeList[i];
+          let node = nodeList[i]
           if (node.attributes.length === 1 && node.style.length !== 0 && node.getAttribute('style').match(pattern[cssName]) !== null) {
             if (node.children.length === 0) {
               if (node.style.length === 1) {
-                let parent = node.parentNode;
-                parent.replaceChild(document.createTextNode(node.innerHTML), node);
-                parent.normalize();
-                i--;
+                let parent = node.parentNode
+                parent.replaceChild(document.createTextNode(node.innerHTML), node)
+                parent.normalize()
+                i--
               } else {
-                node.style[cssName] = '';
+                node.style[cssName] = ''
               }
             } else {
-              temp.push(node);
+              temp.push(node)
             }
           }
         }
         if (temp.length !== 0) {
-          this.formatContent(obj, tagName, cssName);
+          this.formatContent(obj, tagName, cssName)
         }
       },
 
       removeFormat (name, value) {
-        document.execCommand(name, false, value);
-        let range = this.getRange();
-        if (!range) return;
-        let container = range.commonAncestorContainer;
-        container.nodeType === 3 && (container = container.parentNode);
-        container.tagName.toLowerCase() === 'span' && (container = container.parentNode);
-        this.formatContent(container, 'span', 'verticalAlign');
-        container.normalize();
+        document.execCommand(name, false, value)
+        let range = this.getRange()
+        if (!range) return
+        let container = range.commonAncestorContainer
+        container.nodeType === 3 && (container = container.parentNode)
+        container.tagName.toLowerCase() === 'span' && (container = container.parentNode)
+        this.formatContent(container, 'span', 'verticalAlign')
+        container.normalize()
       },
 
       getRange () {
-        let oSel, oRange;
+        let oSel, oRange
         if (window.getSelection) {
-          oSel = window.getSelection();
+          oSel = window.getSelection()
           if (oSel && oSel.rangeCount !== 0) {
-            oRange = oSel.getRangeAt(0);
+            oRange = oSel.getRangeAt(0)
           }
         }
-        return oRange;
+        return oRange
       },
       setRange (range) {
-        let oSel;
+        let oSel
         if (window.getSelection) {
-          oSel = window.getSelection();
-          oSel.removeAllRanges();
-          oSel.addRange(range);
+          oSel = window.getSelection()
+          oSel.removeAllRanges()
+          oSel.addRange(range)
         }
       },
 
       removeRange () {
-        let oSel;
+        let oSel
         if (window.getSelection) {
-          oSel = window.getSelection();
-          oSel.removeAllRanges();
+          oSel = window.getSelection()
+          oSel.removeAllRanges()
         }
       },
 
       rangeValid () {
-        let range = this.getRange();
-        return (range && !range.collapsed);
+        let range = this.getRange()
+        return (range && !range.collapsed)
       },
       unlink () {
-        let range = this.getRange();
-        let container = range.commonAncestorContainer;
+        let range = this.getRange()
+        let container = range.commonAncestorContainer
         if (container.nodeType === 3) {
-          container = container.parentNode;
+          container = container.parentNode
         }
         if (container.tagName === 'A') {
-          let newRange = document.createRange();
-          newRange.selectNode(container);
-          this.setRange(newRange);
-          this.exec('Unlink', null);
-          this.removeRange(newRange);
+          let newRange = document.createRange()
+          newRange.selectNode(container)
+          this.setRange(newRange)
+          this.exec('Unlink', null)
+          this.removeRange(newRange)
         } else {
-          this.exec('Unlink', null);
+          this.exec('Unlink', null)
         }
       }
 
@@ -285,10 +285,10 @@
 </script>
 <style lang="scss" scoped>
   .design > div {
-    height: 100%;
-    outline: none;
-    overflow-y: auto;
-    padding: 8px;
-    box-sizing: border-box;
+    height: 100%
+    outline: none
+    overflow-y: auto
+    padding: 8px
+    box-sizing: border-box
   }
 </style>
