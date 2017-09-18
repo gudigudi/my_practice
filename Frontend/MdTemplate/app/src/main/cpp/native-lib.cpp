3a8f304eba94c24c4281bb08fc9c2942fc38d808
Jiade Dai <huolingdfsq@gmail.com>
2017-09-19 00:41:55 +0800

    update: add new JNI code for MainActivity

--- Frontend/MdTemplate/app/src/main/cpp/native-lib.cpp
+++ Frontend/MdTemplate/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,30 @@
 #include <jni.h>
 #include <string>
 
+#include "Log.h"
+
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_gudigudigudi_mdtemplate_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+    char TAG[]="com_gudigudigudi_mdtemplate_MainActivity";
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+
+    Log::Verbose(TAG,"JNI %s","com_gudigudigudi_mdtemplate_MainActivity_stringFromJNI");
+}
+
+JNIEXPORT void JNICALL
+Java_com_gudigudigudi_mdtemplate_MainActivity_nativeInit(JNIEnv *env, jobject instance) {
+
+
+
+}
+
+JNIEXPORT void JNICALL
+Java_com_gudigudigudi_mdtemplate_MainActivity_nativeFree(JNIEnv *env, jobject instance) {
+
+
 }
