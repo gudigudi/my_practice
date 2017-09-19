75d0700fe47dc8cd1a3203294cda6067f1ce4cd0
Jiade Dai <huolingdfsq@gmail.com>
2017-09-20 00:08:24 +0800

    update: add Activity on-method example: onSaveInstanceState: be called before onDestory() to save activity data.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/MainActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/MainActivity.java
@@ -2,6 +2,7 @@ package com.gudigudigudi.mdtemplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +17,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            String tmpData = savedInstanceState.getString("data_key");
+            Log.d(TAG, "tmpData : " + tmpData);
+        }
 
         // Example of a call to a native method
         Button btn = (Button) findViewById(R.id.button1);
         btn.setText(stringFromJNI());
+    }
+
+
+    /**
+     * 避免activity被回收后, 数据无法保存.
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tmpData = "Something you just typed.";
+        Log.d(TAG, "onSaveInstanceState: " + tmpData);
+        outState.putString("data_key", tmpData);
     }
 
     /**
