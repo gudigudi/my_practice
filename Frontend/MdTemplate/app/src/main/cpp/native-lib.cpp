bd4bdca39506602ceaae64aed5a76e1a91bf0776
Jiade Dai <huolingdfsq@gmail.com>
2017-09-24 21:13:33 +0800

    update: make JNI function stringFromJNI as as function to return random string

--- Frontend/MdTemplate/app/src/main/cpp/native-lib.cpp
+++ Frontend/MdTemplate/app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "Log.h"
+#include "RandomStringGenerator.h"
 
 
 extern "C"
@@ -10,7 +11,7 @@ Java_com_gudigudigudi_mdtemplate_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     char TAG[]="com_gudigudigudi_mdtemplate_MainActivity";
-    std::string hello = "Hello from C++";
+    std::string hello = getRandomString(10);
     return env->NewStringUTF(hello.c_str());
 
     Log::Verbose(TAG,"JNI %s","com_gudigudigudi_mdtemplate_MainActivity_stringFromJNI");
@@ -18,13 +19,8 @@ Java_com_gudigudigudi_mdtemplate_MainActivity_stringFromJNI(
 
 JNIEXPORT void JNICALL
 Java_com_gudigudigudi_mdtemplate_MainActivity_nativeInit(JNIEnv *env, jobject instance) {
-
-
-
 }
 
 JNIEXPORT void JNICALL
 Java_com_gudigudigudi_mdtemplate_MainActivity_nativeFree(JNIEnv *env, jobject instance) {
-
-
 }
