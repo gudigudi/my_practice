f468b9db568f30abf391b104270d771dccb1b7f6
Jiade Dai <huolingdfsq@gmail.com>
2017-09-25 23:10:25 +0800

    update: add custom model/view xml/adapter for fruit listview

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/Fruit.java
@@ -0,0 +1,23 @@
+package com.gudigudigudi.mdtemplate;
+
+/**
+ * Fruit: model for fruit listview
+ */
+public class Fruit {
+
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/FruitAdapter.java
@@ -0,0 +1,38 @@
+package com.gudigudigudi.mdtemplate;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+    public FruitAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull Fruit[] objects) {
+        super(context, resource, objects);
+
+        resourceId = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+        Fruit fruit = getItem(position); // get current item
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+
+        return view;
+    }
+}
