fb75ea235a3defa196ace7cf8c057b6857131dd3
Jiade Dai <huolingdfsq@gmail.com>
2017-09-27 00:38:09 +0800

    update: add recyclerview adapter for Msg.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/MsgAdapter.java
@@ -0,0 +1,71 @@
+package com.gudigudigudi.mdtemplate;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by gu on 9/27/17.
+ */
+
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+
+    private List<Msg> msgList;
+
+    public MsgAdapter(List<Msg> msgList) {
+        this.msgList = msgList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup group, int i) {
+        View view = LayoutInflater.from(group.getContext())
+                .inflate(R.layout.msg_item, group, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int i) {
+        Msg msg = msgList.get(i);
+        if (msg.getType() == Msg.TYPE_RECEIVED) {
+            // 如果消息已读，则显示左边的消息布局，隐藏右边的消息布局
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.leftMsg.setText(msg.getContent());
+        } else if (msg.getType() == Msg.TYPE_SENT) {
+            // 如果消息已发，则显示右边的消息布局，隐藏左边的消息布局
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.rightMsg.setText(msg.getContent());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+//        return 0;
+        return msgList.size();
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            leftLayout = (LinearLayout) itemView.findViewById(R.id.left_layout);
+            rightLayout = (LinearLayout) itemView.findViewById(R.id.right_layout);
+            leftMsg = (TextView) itemView.findViewById(R.id.left_msg);
+            rightMsg = (TextView) itemView.findViewById(R.id.right_msg);
+        }
+    }
+}
