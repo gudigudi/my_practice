e4087f69d88cc917c1463da503caa4402733214f
Jiade Dai <huolingdfsq@gmail.com>
2017-09-28 19:57:28 +0800

    update: add model: News

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/News.java
@@ -0,0 +1,30 @@
+package com.gudigudigudi.mdtemplate;
+
+/**
+ * Created by gu on 9/28/17.
+ */
+
+/**
+ * 新闻实体类
+ */
+public class News {
+
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
