4742f6ec84b0476ad195c23a2920ea37b18d7c1c
Jiade Dai <huolingdfsq@gmail.com>
2017-10-02 16:41:37 +0800

    update: add file reader and writer for DataStoreActivity.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/DataStoreActivity.java
@@ -0,0 +1,97 @@
+package com.gudigudigudi.mdtemplate;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class DataStoreActivity extends AppCompatActivity {
+
+    private static final String TAG = "DataStoreActivity";
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_store);
+
+        editText = (EditText) findViewById(R.id.edit);
+
+        String inputText = load();
+
+        if (!TextUtils.isEmpty(inputText)) {
+            editText.setText(inputText);
+            editText.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        String input = editText.getText().toString();
+        save(input);
+    }
+
+    private void save(String input) {
+
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String load() {
+
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return content.toString();
+    }
+
+}
