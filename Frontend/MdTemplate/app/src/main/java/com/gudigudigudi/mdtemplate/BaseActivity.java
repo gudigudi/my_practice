0350dc3194ddce6a7468240711b51f51633473e3
Jiade Dai <huolingdfsq@gmail.com>
2017-10-02 15:37:44 +0800

    update: add inline force offline broadcast for BaseActivity.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/BaseActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/BaseActivity.java
@@ -1,13 +1,22 @@
 package com.gudigudigudi.mdtemplate;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class BaseActivity extends AppCompatActivity {
 
     private final static String TAG = "BaseActivity";
+
+    private ForceOfflineReceiver receiver;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,9 +28,50 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(getString(R.string.FORCE_OFFLINE_BROADCAST));
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
 
         ActivityCollector.removeActivity(this);
+    }
+
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning")
+                    .setMessage("You are forced to be offline. Please try to login again.")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface anInterface, int i) {
+                            ActivityCollector.finishAll();
+                            Intent intent = new Intent(context, LoginActivity.class);
+                            context.startActivity(intent);
+                        }
+                    }).show();
+        }
     }
 }
