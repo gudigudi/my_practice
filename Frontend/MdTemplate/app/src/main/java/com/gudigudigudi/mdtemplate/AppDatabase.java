dce1e63e8be7c5d4f3650f5ab953c3ae1c230acc
Jiade Dai <huolingdfsq@gmail.com>
2017-10-03 13:10:53 +0800

    fix: add Entity category to AppDatabase.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/AppDatabase.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/AppDatabase.java
@@ -7,7 +7,7 @@ import android.arch.persistence.room.RoomDatabase;
  * Created by gu on 10/3/17.
  */
 
-@Database(entities = {Book.class}, version = 3)
+@Database(entities = {Book.class, Category.class}, version = 3)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract BookDao bookDao();
