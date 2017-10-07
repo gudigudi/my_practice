2236e0278289fdabb0720e6bfbc663864aad593e
Jiade Dai <huolingdfsq@gmail.com>
2017-10-08 01:10:49 +0800

    update: add menu items for toolbar.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/MaterialDesignActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/MaterialDesignActivity.java
@@ -3,8 +3,13 @@ package com.gudigudigudi.mdtemplate;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MaterialDesignActivity extends AppCompatActivity {
+    private static final String TAG = "MaterialDesignActivity";
 
     private Toolbar toolbar;
 
@@ -15,5 +20,30 @@ public class MaterialDesignActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.backup:
+                Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Log.d(TAG, "Unknown menu item is clicked");
+                break;
+        }
+        return true;
     }
 }
