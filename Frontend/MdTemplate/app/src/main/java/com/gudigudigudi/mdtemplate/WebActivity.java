7c5273f69704b807807457ec515373db4c447df6
Jiade Dai <huolingdfsq@gmail.com>
2017-10-07 13:07:53 +0800

    update: add XML parser example for okhttp response body.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/WebActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/WebActivity.java
@@ -3,16 +3,22 @@ package com.gudigudigudi.mdtemplate;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -48,6 +54,7 @@ public class WebActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                sendRequestWithHttpURLConnection();
                 sendRequestWithOkHttp();
+                sendRequestGetXMLWithOkHttp();
             }
         });
 
@@ -70,21 +77,13 @@ public class WebActivity extends AppCompatActivity {
                     connection.setReadTimeout(8000);
 
                     InputStream in = connection.getInputStream();
-
                     reader = new BufferedReader(new InputStreamReader(in));
-                    final StringBuilder response = new StringBuilder();
+                    StringBuilder response = new StringBuilder();
                     String line;
                     while ((line = reader.readLine()) != null) {
                         response.append(line);
                     }
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            tv_response_text.setText(response.toString());
-                        }
-                    });
-
+                    showResponse(response.toString());
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
@@ -106,29 +105,86 @@ public class WebActivity extends AppCompatActivity {
     }
 
     private void sendRequestWithOkHttp() {
-        new AsyncTask<Void, Void, Void>() {
-
+        new Thread(new Runnable() {
             @Override
-            protected Void doInBackground(Void... voids) {
+            public void run() {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder().url("https://www.baidu.com").build();
-                    Response response = null;
-                    response = client.newCall(request).execute();
-                    final String responseBody = response.body().string();
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            tv_response_text.setText(responseBody);
-                        }
-                    });
-                } catch (IOException e) {
+                    Response response = client.newCall(request).execute();
+                    showResponse(response.body().string());
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-                return null;
             }
-        }.execute();
+        }).start();
+    }
+
+    private void showResponse(final String responseData) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tv_response_text.setText(responseData);
+            }
+        });
+    }
+
+    private void sendRequestGetXMLWithOkHttp() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url("http://114.215.134.219/get_data.xml").build();
+                    Response response = client.newCall(request).execute();
+                    parseXMLWithPull(response.body().string());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new StringReader(xmlData));
+
+            int eventType = parser.getEventType();
+
+            String id = "";
+            String name = "";
+            String version = "";
+
+            while (eventType != parser.END_DOCUMENT) {
+                String nodeName = parser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if ("id".equals(nodeName)) {
+                            id = parser.nextText();
+                        } else if ("name".equals(nodeName)) {
+                            name = parser.nextText();
+                        } else if ("version".equals(nodeName)) {
+                            version = parser.nextText();
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if ("app".equals(nodeName)) {
+                            Log.d(TAG, "id is: " + id);
+                            Log.d(TAG, "name is: " + name);
+                            Log.d(TAG, "version is: " + version);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                eventType = parser.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
