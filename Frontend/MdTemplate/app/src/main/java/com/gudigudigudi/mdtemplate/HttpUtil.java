0539712db42ba097e70c7a90b340133c9ddbea20
Jiade Dai <huolingdfsq@gmail.com>
2017-10-07 23:36:47 +0800

    update: add sendOkHttpRequest method.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/HttpUtil.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/HttpUtil.java
@@ -5,6 +5,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+
 /**
  * Created by gu on 10/7/17.
  */
@@ -14,6 +19,11 @@ public class HttpUtil {
     private static final int CONNECTION_TIMEOUT = 8000;
     private static final int READ_TIMEOUT = 8000;
 
+    public static void sendOkHttpRequest(String address, Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 
     public static void sendHttpRequest(String address, final HttpCallbackListener listener) {
         HttpURLConnection connection = null;
