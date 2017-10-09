3df1d955dac2869e54981099a1006af87dc0a356
Jiade Dai <huolingdfsq@gmail.com>
2017-10-10 00:38:28 +0800

    update: add LogUtil, with Log templates.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/LogUtil.java
@@ -0,0 +1,13 @@
+package com.gudigudigudi.mdtemplate;
+
+/**
+ * Created by gu on 10/9/17.
+ */
+
+public class LogUtil {
+
+    public static final String LOG_VIEW_IS_CLICKED = "View %s is clicked";
+    public static final String LOG_UNKNOWN_VIEW_IS_CLICKED = "Unknown View is clicked";
+    public static final String LOG_UNKNOWN_REQUEST_CODE = "Unknown request code";
+
+}
