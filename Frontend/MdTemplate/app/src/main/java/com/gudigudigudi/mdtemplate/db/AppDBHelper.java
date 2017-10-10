b810d6a3b868bab8e41e24ed50639c7091703315
Jiade Dai <huolingdfsq@gmail.com>
2017-10-11 00:44:38 +0800

    rename DBHelper.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/db/AppDBHelper.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/db/AppDBHelper.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 /**
  * Book sqlite db.
  */
-public class BookDBHelper extends SQLiteOpenHelper {
+public class AppDBHelper extends SQLiteOpenHelper {
 
     public static final String CREATE_SQL = "CREATE TABLE book (" +
             "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -26,25 +26,39 @@ public class BookDBHelper extends SQLiteOpenHelper {
             "name TEXT, " +
             "code INTEGER )";
 
+    public static final String CREATE_CITY = "CREATE TABLE city (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "name TEXT, " +
+            "code INTEGER, " +
+            "provinceId INTEGER )";
+    public static final String CREATE_COUNTY = "CREATE TABLE county (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "name TEXT, " +
+            "weatherId TEXT, " +
+            "cityId INTEGER )";
+    public static final String CREATE_PROVINCE = "CREATE TABLE province (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "name TEXT, " +
+            "code INTEGER )";
 
     private Context context;
 
-
-    public BookDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public AppDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
 
         this.context = context;
     }
-
 
     @Override
     public void onCreate(SQLiteDatabase database) {
 
         database.execSQL(CREATE_SQL);
         database.execSQL(CREATE_CATEGORY);
+        database.execSQL(CREATE_CITY);
+        database.execSQL(CREATE_COUNTY);
+        database.execSQL(CREATE_PROVINCE);
 
         Toast.makeText(context, "Create database succeeded", Toast.LENGTH_SHORT).show();
-
     }
 
     @Override
@@ -52,8 +66,10 @@ public class BookDBHelper extends SQLiteOpenHelper {
 
         database.execSQL("DROP TABLE IF EXISTS book");
         database.execSQL("DROP TABLE IF EXISTS category");
+        database.execSQL("DROP TABLE IF EXISTS city");
+        database.execSQL("DROP TABLE IF EXISTS county");
+        database.execSQL("DROP TABLE IF EXISTS province");
 
         onCreate(database);
-
     }
 }
