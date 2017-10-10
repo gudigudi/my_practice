ea3b31868940307faaab84756e08cc7c5b4c9f5f
Jiade Dai <huolingdfsq@gmail.com>
2017-10-11 00:49:28 +0800

    update: make DBHelper global.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/BaseApplication.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/BaseApplication.java
@@ -3,6 +3,7 @@ package com.gudigudigudi.mdtemplate;
 import android.app.Application;
 import android.content.Context;
 
+import com.gudigudigudi.mdtemplate.db.AppDBHelper;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.DiskLogAdapter;
 import com.orhanobut.logger.Logger;
@@ -16,6 +17,10 @@ public class BaseApplication extends Application {
     // Global context of Application.
     private static Context context;
 
+    private AppDBHelper appDBHelper;
+    public static final String APP_DB_NAME = "app.db";
+    public static final int APP_DB_VERSION = 1;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -25,6 +30,8 @@ public class BaseApplication extends Application {
         Logger.addLogAdapter(new AndroidLogAdapter());
         Logger.addLogAdapter(new DiskLogAdapter());
 
+//        appDBHelper= new AppDBHelper(context, APP_DB_NAME,null,APP_DB_VERSION);
+//        appDBHelper.getWritableDatabase();
     }
 
     public static Context getContext() {
