d7233f0ed8f642cb48d186dc5b4e6d5f9bc85aed
Jiade Dai <huolingdfsq@gmail.com>
2017-10-12 23:43:04 +0800

    update: add ChooseAreaActivity as container of ChooseAreaFragment.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/ChooseAreaActivity.java
@@ -0,0 +1,29 @@
+package com.gudigudigudi.mdtemplate;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class ChooseAreaActivity extends AppCompatActivity implements ChooseAreaFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_area);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
