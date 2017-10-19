d81360bedc81b50e3c6c873fa16f6a05ec29db6c
Jiade Dai <huolingdfsq@gmail.com>
2017-10-19 22:34:47 +0800

    update: add PagerTitleStrip for ViewPager.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/materialdesign/ViewPagerActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/materialdesign/ViewPagerActivity.java
@@ -1,6 +1,7 @@
 package com.gudigudigudi.mdtemplate.activity.materialdesign;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -14,8 +15,10 @@ import java.util.List;
 
 public class ViewPagerActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
     private List<View> viewList;
+    private List<String> titleList;
+    private ViewPager viewPager;
+    private PagerTitleStrip pagerTitleStrip;
     private ViewPagerAdapter viewPagerAdapter;
 
     @Override
@@ -24,15 +27,18 @@ public class ViewPagerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_pager);
 
         viewPager = (ViewPager) findViewById(R.id.vp);
+        pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pts);
 
         initPagers();
 
-        viewPagerAdapter = new ViewPagerAdapter(viewList);
+        viewPagerAdapter = new ViewPagerAdapter(viewList, titleList);
         viewPager.setAdapter(viewPagerAdapter);
     }
 
     private void initPagers() {
         viewList = new ArrayList<>();
+        titleList = new ArrayList<>();
+
         View view1 = LayoutInflater.from(this).inflate(R.layout.view_pager_layout1, null);
         View view2 = LayoutInflater.from(this).inflate(R.layout.view_pager_layout1, null);
         View view3 = LayoutInflater.from(this).inflate(R.layout.view_pager_layout1, null);
@@ -40,5 +46,9 @@ public class ViewPagerActivity extends AppCompatActivity {
         viewList.add(view1);
         viewList.add(view2);
         viewList.add(view3);
+
+        titleList.add("page 1");
+        titleList.add("page 2");
+        titleList.add("page 3");
     }
 }
