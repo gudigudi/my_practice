30a7411c6d6505379a6c423bacaaa78844f9feb9
Jiade Dai <huolingdfsq@gmail.com>
2017-10-19 22:48:02 +0800

    update: add PagerTabStrip for ViewPager.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/materialdesign/ViewPagerActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/materialdesign/ViewPagerActivity.java
@@ -1,7 +1,8 @@
 package com.gudigudigudi.mdtemplate.activity.materialdesign;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.view.PagerTitleStrip;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -18,8 +19,9 @@ public class ViewPagerActivity extends AppCompatActivity {
     private List<View> viewList;
     private List<String> titleList;
     private ViewPager viewPager;
-    private PagerTitleStrip pagerTitleStrip;
     private ViewPagerAdapter viewPagerAdapter;
+    //    private PagerTitleStrip pagerTitleStrip;
+    private PagerTabStrip pagerTabStrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,12 @@ public class ViewPagerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_pager);
 
         viewPager = (ViewPager) findViewById(R.id.vp);
-        pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pts);
+//        pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pager_title_strip);
+        pagerTabStrip = (PagerTabStrip) findViewById(R.id.pager_tab_strip);
+        pagerTabStrip.setBackgroundColor(Color.BLUE);
+        pagerTabStrip.setTabIndicatorColor(Color.RED);
+        pagerTabStrip.setTextColor(Color.GREEN);
+        pagerTabStrip.setTextSize(1, 24);
 
         initPagers();
 
@@ -42,13 +49,16 @@ public class ViewPagerActivity extends AppCompatActivity {
         View view1 = LayoutInflater.from(this).inflate(R.layout.view_pager_layout1, null);
         View view2 = LayoutInflater.from(this).inflate(R.layout.view_pager_layout1, null);
         View view3 = LayoutInflater.from(this).inflate(R.layout.view_pager_layout1, null);
+        View view4 = LayoutInflater.from(this).inflate(R.layout.view_pager_layout1, null);
 
         viewList.add(view1);
         viewList.add(view2);
         viewList.add(view3);
+        viewList.add(view4);
 
         titleList.add("page 1");
         titleList.add("page 2");
         titleList.add("page 3");
+        titleList.add("page 4");
     }
 }
