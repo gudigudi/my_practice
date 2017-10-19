001a0e16374a6ea90c7f5899588c2706a1970a3d
Jiade Dai <huolingdfsq@gmail.com>
2017-10-19 23:21:39 +0800

    update: add remove/hide fragment demo.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/FragmentDemoActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/FragmentDemoActivity.java
@@ -11,23 +11,32 @@ import com.gudigudigudi.mdtemplate.base.BaseActivity;
 import com.gudigudigudi.mdtemplate.fragment.LeftFragment;
 import com.gudigudigudi.mdtemplate.fragment.Right2Fragment;
 import com.gudigudigudi.mdtemplate.fragment.RightFragment;
+import com.gudigudigudi.mdtemplate.util.LogUtil;
+import com.orhanobut.logger.Logger;
 
-public class FragmentDemoActivity extends BaseActivity implements LeftFragment.OnFragmentInteractionListener, RightFragment.OnFragmentInteractionListener, Right2Fragment.OnFragmentInteractionListener {
+public class FragmentDemoActivity extends BaseActivity implements View.OnClickListener,
+        LeftFragment.OnFragmentInteractionListener, RightFragment.OnFragmentInteractionListener,
+        Right2Fragment.OnFragmentInteractionListener {
 
     private final static String TAG = "FragmentDemoActivity";
+
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_demo);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                replaceFragment(new Right2Fragment());
-            }
-        });
+        // Button in LeftFragment.
+        Button btn_replace = (Button) findViewById(R.id.replace);
+        Button btn_remove = (Button) findViewById(R.id.remove);
+        Button btn_hide = (Button) findViewById(R.id.hide);
+
+        fragment = new Right2Fragment();
+
+        btn_replace.setOnClickListener(this);
+        btn_remove.setOnClickListener(this);
+        btn_hide.setOnClickListener(this);
 
         replaceFragment(new RightFragment());
     }
@@ -37,18 +46,39 @@ public class FragmentDemoActivity extends BaseActivity implements LeftFragment.O
 
     }
 
-    private void replaceFragment(Fragment fragment) {
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction transaction = fragmentManager.beginTransaction();
-//        transaction.replace(R.id.right_fragment_layout, fragment);
-//        // Enter back key, and go back to last fragment.
-//        transaction.addToBackStack(null);
-//
-//        transaction.commit();
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.replace:
+                replaceFragment(fragment);
+                break;
+            case R.id.remove:
+                removeFragment(fragment);
+                break;
+            case R.id.hide:
+                hideFragment(fragment);
+                break;
+            default:
+                Logger.d(LogUtil.LOG_UNKNOWN_VIEW_IS_CLICKED);
+        }
+    }
 
+    private void replaceFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.right_fragment_layout, fragment)
                 .addToBackStack(null)
+                .commit();
+    }
+
+    private void removeFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .remove(fragment)
+                .commit();
+    }
+
+    private void hideFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .hide(fragment)
                 .commit();
     }
 }
