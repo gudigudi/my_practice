b6e886497cf1db8653be3dcb8d53d09c1dd3c219
Jiade Dai <huolingdfsq@gmail.com>
2017-10-22 22:11:24 +0800

    update: demo: get sms content by broadcast receiver.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/SmsBroadcastActivity.java
@@ -0,0 +1,39 @@
+package com.gudigudigudi.mdtemplate.activity;
+
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.gudigudigudi.mdtemplate.R;
+import com.gudigudigudi.mdtemplate.receiver.SmsBroadcastReceiver;
+import com.orhanobut.logger.Logger;
+
+public class SmsBroadcastActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private SmsBroadcastReceiver smsBroadcastReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_broadcast);
+
+        textView = (TextView) findViewById(R.id.text_view);
+        smsBroadcastReceiver = new SmsBroadcastReceiver();
+
+        init();
+
+        registerReceiver(smsBroadcastReceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+        Logger.d("registerReceiver");
+    }
+
+    private void init() {
+        smsBroadcastReceiver.setOnReceiveMessageListener(new SmsBroadcastReceiver.MessageListener() {
+            @Override
+            public void OnReceived(String message) {
+                textView.setText(message);
+            }
+        });
+    }
+}
