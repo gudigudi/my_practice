179ede4dd12e6c14c1376d6dc562d26bf70aff7f
Jiade Dai <huolingdfsq@gmail.com>
2017-10-29 23:14:43 +0800

    update: add rotate and scale animation example.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/AnimationActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/AnimationActivity.java
@@ -9,11 +9,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.gudigudigudi.mdtemplate.R;
+import com.gudigudigudi.mdtemplate.util.LogUtil;
+import com.orhanobut.logger.Logger;
 
-public class AnimationActivity extends AppCompatActivity {
+
+public class AnimationActivity extends AppCompatActivity implements Button.OnClickListener {
 
     private ImageView imageView;
     private Button btn_start, btn_cancel;
+
+    private Button btn_rotate;
+    private ImageView iv_rotate;
+
+    private Button btn_scale;
+    private ImageView iv_scale;
+
+    private Animation alphaAnimation, rotateAnimation, scaleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +34,41 @@ public class AnimationActivity extends AppCompatActivity {
         imageView = findViewById(R.id.image_view);
         btn_start = findViewById(R.id.btn_start);
         btn_cancel = findViewById(R.id.btn_cancel);
+        btn_rotate = findViewById(R.id.btn_rotate);
+        iv_rotate = findViewById(R.id.iv_rotate);
+        btn_scale = findViewById(R.id.btn_scale);
+        iv_scale = findViewById(R.id.iv_scale);
 
         imageView.setBackgroundResource(R.mipmap.ic_launcher);
 
-        final Animation animation = AnimationUtils.loadAnimation(AnimationActivity.this, R.anim.alpha);
+        alphaAnimation = AnimationUtils.loadAnimation(AnimationActivity.this, R.anim.alpha);
+        rotateAnimation = AnimationUtils.loadAnimation(AnimationActivity.this, R.anim.rotate);
+        scaleAnimation = AnimationUtils.loadAnimation(AnimationActivity.this, R.anim.scale);
 
-        btn_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.startAnimation(animation);
-            }
-        });
-        btn_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                animation.cancel();
-            }
-        });
+
+        btn_start.setOnClickListener(this);
+        btn_cancel.setOnClickListener(this);
+        btn_rotate.setOnClickListener(this);
+        btn_scale.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start:
+                imageView.startAnimation(alphaAnimation);
+                break;
+            case R.id.btn_cancel:
+                alphaAnimation.cancel();
+                break;
+            case R.id.btn_rotate:
+                iv_rotate.startAnimation(rotateAnimation);
+                break;
+            case R.id.btn_scale:
+                iv_scale.startAnimation(scaleAnimation);
+                break;
+            default:
+                Logger.d(LogUtil.LOG_UNKNOWN_VIEW_IS_CLICKED);
+        }
     }
 }
