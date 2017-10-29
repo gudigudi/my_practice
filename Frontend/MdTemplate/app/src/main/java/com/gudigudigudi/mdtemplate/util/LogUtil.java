cdb81060b4e9fbc7a20a370d7600053027f581a2
Jiade Dai <huolingdfsq@gmail.com>
2017-10-30 00:01:46 +0800

    update: add log template: unknown menu item is clicked.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/util/LogUtil.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/util/LogUtil.java
@@ -8,6 +8,7 @@ public class LogUtil {
 
     public static final String LOG_VIEW_IS_CLICKED = "View %s is clicked";
     public static final String LOG_UNKNOWN_VIEW_IS_CLICKED = "Unknown View is clicked";
+    public static final String LOG_UNKNOWN_MENU_ITEM_IS_CLICKED = "Unknown Menu item is clicked";
     public static final String LOG_UNKNOWN_REQUEST_CODE = "Unknown request code";
 
 }
