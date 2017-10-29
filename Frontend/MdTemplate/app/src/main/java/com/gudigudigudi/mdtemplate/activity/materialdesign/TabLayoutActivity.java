5ef8cadd1f281f25d3741b1cbdd975618e0804e8
Jiade Dai <huolingdfsq@gmail.com>
2017-10-29 19:07:18 +0800

    update: add TabLayout example: display with viewPager and fragments.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/materialdesign/TabLayoutActivity.java
@@ -0,0 +1,71 @@
+package com.gudigudigudi.mdtemplate.activity.materialdesign;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.gudigudigudi.mdtemplate.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabLayoutActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_layout);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        setSupportActionBar(toolbar);
+
+        final List<String> titles = new ArrayList<>();
+        final List<Fragment> fragmentList = new ArrayList<>();
+
+        titles.add("精选");
+        titles.add("体育");
+        titles.add("巴萨");
+        titles.add("购物");
+        titles.add("明星");
+        titles.add("视频");
+        titles.add("健康");
+        titles.add("励志");
+        titles.add("图文");
+        titles.add("本地");
+        titles.add("动漫");
+        titles.add("搞笑");
+        titles.add("精选");
+
+        for (int i = 0; i < titles.size(); i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(titles.get(i)));
+            fragmentList.add(new ListFragment()); //TODO: replace with custom listfragment.
+        }
+
+        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return fragmentList.get(i);
+            }
+
+            @Override
+            public int getCount() {
+                return fragmentList.size();
+            }
+        };
+
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
