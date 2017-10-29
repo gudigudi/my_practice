e79976dc5a7355780802076c62a8de643e180a1f
Jiade Dai <huolingdfsq@gmail.com>
2017-10-29 19:08:00 +0800

    update: add custom listfragment.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/materialdesign/ListFragment.java
@@ -0,0 +1,35 @@
+package com.gudigudigudi.mdtemplate.activity.materialdesign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gudigudigudi.mdtemplate.R;
+
+/**
+ * Created by gu on 10/21/17.
+ */
+
+public class ListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_list, container, false);
+        return recyclerView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+//        recyclerView.setAdapter();
+    }
+}
