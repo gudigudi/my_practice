266d60a12cebe83412d3401291765741182d3b63
Jiade Dai <huolingdfsq@gmail.com>
2017-10-29 16:28:21 +0800

    fix: add annotation 'Override' for onCreate() method.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/DownloadActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/DownloadActivity.java
@@ -38,6 +38,7 @@ public class DownloadActivity extends AppCompatActivity implements View.OnClickL
         }
     };
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
