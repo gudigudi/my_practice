17e75be425c5243916ac45a6ca873cca6d6ffc47
Jiade Dai <huolingdfsq@gmail.com>
2017-10-30 23:23:43 +0800

    update: add Interpolators for RotateAnimation: 1. AccelerateDecelerateInterpolator 2. AccelerateInterpolator 3. DecelerateInterpolator 4. BounceInterpolator 5. CycleInterpolator 6. LinearInterpolator

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/AnimationActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/AnimationActivity.java
@@ -3,8 +3,14 @@ package com.gudigudigudi.mdtemplate.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -28,6 +34,7 @@ public class AnimationActivity extends AppCompatActivity implements Button.OnCli
     private ImageView iv_translate;
 
     private Animation alphaAnimation, rotateAnimation, scaleAnimation, translateAnimation;
+    private Button btn_accelerate_decelerate_interpolator, btn_accelerate_interpolator, btn_decelerate_interpolator, btn_bounce_interpolator, btn_cycle_interpolator, btn_linear_interpolator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,12 @@ public class AnimationActivity extends AppCompatActivity implements Button.OnCli
         iv_scale = findViewById(R.id.iv_scale);
         btn_translate = findViewById(R.id.btn_translate);
         iv_translate = findViewById(R.id.iv_translate);
+        btn_accelerate_decelerate_interpolator = findViewById(R.id.btn_accelerate_decelerate_interpolator);
+        btn_accelerate_interpolator = findViewById(R.id.btn_accelerate_interpolator);
+        btn_decelerate_interpolator = findViewById(R.id.btn_decelerate_interpolator);
+        btn_bounce_interpolator = findViewById(R.id.btn_bounce_interpolator);
+        btn_cycle_interpolator = findViewById(R.id.btn_cycle_interpolator);
+        btn_linear_interpolator = findViewById(R.id.btn_linear_interpolator);
 
         imageView.setBackgroundResource(R.mipmap.ic_launcher);
 
@@ -56,6 +69,12 @@ public class AnimationActivity extends AppCompatActivity implements Button.OnCli
         btn_rotate.setOnClickListener(this);
         btn_scale.setOnClickListener(this);
         btn_translate.setOnClickListener(this);
+        btn_accelerate_decelerate_interpolator.setOnClickListener(this);
+        btn_accelerate_interpolator.setOnClickListener(this);
+        btn_decelerate_interpolator.setOnClickListener(this);
+        btn_bounce_interpolator.setOnClickListener(this);
+        btn_cycle_interpolator.setOnClickListener(this);
+        btn_linear_interpolator.setOnClickListener(this);
     }
 
     @Override
@@ -75,6 +94,30 @@ public class AnimationActivity extends AppCompatActivity implements Button.OnCli
                 break;
             case R.id.btn_translate:
                 iv_translate.startAnimation(translateAnimation);
+                break;
+            case R.id.btn_accelerate_decelerate_interpolator:
+                rotateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+                iv_rotate.startAnimation(rotateAnimation);
+                break;
+            case R.id.btn_accelerate_interpolator:
+                rotateAnimation.setInterpolator(new AccelerateInterpolator());
+                iv_rotate.startAnimation(rotateAnimation);
+                break;
+            case R.id.btn_decelerate_interpolator:
+                rotateAnimation.setInterpolator(new DecelerateInterpolator());
+                iv_rotate.startAnimation(rotateAnimation);
+                break;
+            case R.id.btn_bounce_interpolator:
+                rotateAnimation.setInterpolator(new BounceInterpolator());
+                iv_rotate.startAnimation(rotateAnimation);
+                break;
+            case R.id.btn_cycle_interpolator:
+                rotateAnimation.setInterpolator(new CycleInterpolator(3.0f));
+                iv_rotate.startAnimation(rotateAnimation);
+                break;
+            case R.id.btn_linear_interpolator:
+                rotateAnimation.setInterpolator(new LinearInterpolator());
+                iv_rotate.startAnimation(rotateAnimation);
                 break;
             default:
                 Logger.d(LogUtil.LOG_UNKNOWN_VIEW_IS_CLICKED);
