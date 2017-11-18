759def45b1d784d86adaae28ed41b4569d539711
Jiade Dai <huolingdfsq@gmail.com>
2017-11-18 16:53:15 +0800

    add commnet for BaseApplication: what should BaseApplication do.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/base/BaseApplication.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/base/BaseApplication.java
@@ -12,6 +12,10 @@ import com.orhanobut.logger.Logger;
 
 /**
  * Created by gu on 10/9/17.
+ * 初始化SDK和应用需要的库.
+ * 注册动态broadcast receiver.
+ * 创建管理service.
+ * 管理应用的实际开始点.
  */
 
 public class BaseApplication extends Application {
