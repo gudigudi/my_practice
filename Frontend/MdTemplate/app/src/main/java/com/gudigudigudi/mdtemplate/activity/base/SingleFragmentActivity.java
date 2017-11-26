bcd771c79231e00c9d1f3a678fd38e2b2c854027
Jiade Dai <huolingdfsq@gmail.com>
2017-11-27 00:05:57 +0800

    add abstract Activity with a fragment.

---
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/activity/base/SingleFragmentActivity.java
@@ -0,0 +1,37 @@
+package com.gudigudigudi.mdtemplate.activity.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+import com.gudigudigudi.mdtemplate.R;
+
+/**
+ * Created by gu on 11/26/17.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+
+}
