fa602ecf3ced8402ec0e42d2fdf604e3bcc0d0a7
Jiade Dai <huolingdfsq@gmail.com>
2017-12-03 21:20:27 +0800

    update comment style in models.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/model/Msg.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/model/Msg.java
@@ -12,8 +12,10 @@ public class Msg {
     public static final int TYPE_RECEIVED = 0;
     public static final int TYPE_SENT = 1;
 
-    private String content; // 消息内容
-    private int type; // 消息类型
+    // 消息内容
+    private String content;
+    // 消息类型
+    private int type;
 
     public Msg(String content, int type) {
         this.content = content;
