32ba856e5b9624862aa9331e9a50a87111095900
Jiade Dai <huolingdfsq@gmail.com>
2017-12-10 22:32:25 +0800

    clean code : remove unused methods.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/base/BaseActivity.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/base/BaseActivity.java
@@ -9,18 +9,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
-import com.gudigudigudi.mdtemplate.ActivityCollector;
 import com.gudigudigudi.mdtemplate.R;
 import com.gudigudigudi.mdtemplate.activity.LoginActivity;
+import com.gudigudigudi.mdtemplate.collector.ActivityCollector;
 import com.orhanobut.logger.Logger;
 
 public class BaseActivity extends AppCompatActivity {
 
     private final static String TAG = "BaseActivity";
-
-    private long mExitTime;
 
     private ForceOfflineReceiver receiver;
 
@@ -59,15 +56,9 @@ public class BaseActivity extends AppCompatActivity {
         ActivityCollector.removeActivity(this);
     }
 
-    private void doubleBackToExit() {
-        if ((System.currentTimeMillis() - mExitTime) > 2000) {
-            Toast.makeText(getApplicationContext(), "Click again to leave", Toast.LENGTH_SHORT).show();
-            mExitTime = System.currentTimeMillis();
-        } else {
-            finish();
-        }
-    }
-
+    /**
+     * force offline broadcast-receiver
+     */
     class ForceOfflineReceiver extends BroadcastReceiver {
 
         @Override
