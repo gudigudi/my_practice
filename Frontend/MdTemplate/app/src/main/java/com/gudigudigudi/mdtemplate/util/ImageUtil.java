7d4bcace622291233dd33a66fcccf38b754604bd
gudi <huolingdfsq@gmail.com>
2018-01-07 22:06:59 +0800

    add new api to ImageUtil: resize bitmap to a canvas with required width and height.

--- Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/util/ImageUtil.java
+++ Frontend/MdTemplate/app/src/main/java/com/gudigudigudi/mdtemplate/util/ImageUtil.java
@@ -1,6 +1,8 @@
 package com.gudigudigudi.mdtemplate.util;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
@@ -48,5 +50,38 @@ public class ImageUtil {
         canvas.drawBitmap(bitmap, null, rect, null);
 
         compressQuality(result, 100, file);
+    }
+
+
+    public static Bitmap decodeSampleBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize.
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set.
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int width = options.outWidth;
+        final int height = options.outHeight;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int halfWidth = width / 2;
+            final int halfHeight = height / 2;
+
+            while ((halfWidth / inSampleSize) >= reqWidth &&
+                    (halfHeight / inSampleSize) >= reqHeight) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
