755dbc4399f83276b8a22e01020ccc68e8908ad9
jiade dai <jiade dai>
2019-11-07 09:19:29 +0800

    Frontend/vue2ts: update main.ts, add vue-lazyload

--- Frontend/vue2ts/src/main.ts
+++ Frontend/vue2ts/src/main.ts
@@ -5,13 +5,23 @@ import store from './store';
 import i18n from './i18n';
 import './plugins/element';
 import './plugins/composition_api';
-import service from './utils/https';
+import axios from './utils/https';
 import urls from './utils/urls';
 
-Vue.prototype.$https = service; // 其他页面在使用 axios 的时候直接  this.$http 就可以了
+import VueLazyload from 'vue-lazyload';
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: 'dist/loading.gif',
+  attempt: 1,
+});
+
+Vue.prototype.$https = axios; // 其他页面在使用 axios 的时候直接  this.$http 就可以了
 Vue.prototype.$urls = urls; // 其他页面在使用 urls 的时候直接  this.$urls 就可以了
 
 Vue.config.productionTip = false;
+Vue.config.performance = process.env.NODE_ENV !== 'production';
 
 new Vue({
   router,
