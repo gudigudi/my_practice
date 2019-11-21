d54bcb855805990d283441abe4e038676636df45
jiade dai <jiade dai>
2019-11-21 11:45:48 +0800

    Frontend/MdTemplate: update deps

--- Frontend/MdTemplate/benchmark/src/androidTest/java/com/gudigudigudi/benchmark/ViewBenchmark.java
+++ Frontend/MdTemplate/benchmark/src/androidTest/java/com/gudigudigudi/benchmark/ViewBenchmark.java
@@ -11,6 +11,8 @@ import androidx.benchmark.junit4.BenchmarkRule;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.benchmark.R;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
