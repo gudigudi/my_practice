1c3cc6696b578e45bbf5bdd7b06ac0b59520f82f
jiade dai <huolingdfsq@gmail.com>
2020-02-11 13:15:30 +0800

    Frontend/vue2ts: update& remove deps

--- Frontend/vue2ts/postcss.config.js
+++ Frontend/vue2ts/postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: {
-    autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 100,
-      propWhiteList: [],
-      minPixelValue: 2
-    }
+    autoprefixer: {}
   }
 }
