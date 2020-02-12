36b6e0f7599110ec2adc4af20c39c19808f935f0
jiade dai <huolingdfsq@gmail.com>
2020-02-12 17:07:20 +0800

    Update App.vue

--- Frontend/vue2ts/src/App.vue
+++ Frontend/vue2ts/src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/button-demo">Button Demo</router-link>|
-      <router-link to="/container-demo">Container Demo</router-link>|
-      <router-link to="/form-demo">Form Demo</router-link>|
-      <router-link to="/layout-demo">Layout Demo</router-link>|
-    </div>
     <router-view />
     <p>use v-lazy-container work with raw HTML</p>
     <div v-lazy-container="{ selector: 'img' }">
@@ -28,27 +21,18 @@
   </div>
 </template>
 
-<style scoped>
+<script lang="ts">
+import { Component, Prop, PropSync, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {}
+</script>
+<style lang="scss" scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-  font-size: 32px;
-  line-height: 1.2;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #fc9153;
 }
 </style>
