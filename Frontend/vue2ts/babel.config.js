ddbed26356ab09665e746e14e728b56ed37a5db5
jiade dai <huolingdfsq@gmail.com>
2020-02-12 17:06:59 +0800

    Update babel.config.js
    add antdesign-vue support

--- Frontend/vue2ts/babel.config.js
+++ Frontend/vue2ts/babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: [["@vue/cli-plugin-babel/preset",{useBuiltIns:"entry"}]],
+  presets: [["@vue/cli-plugin-babel/preset", { useBuiltIns: "entry" }]],
+  "plugins": [
+    ["import", {
+      "libraryName": "antd",
+      "libraryDirectory": "es",
+      "style": "css" // `style: true` 会加载 less 文件
+    }]
+  ]
 }
