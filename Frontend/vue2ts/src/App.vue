48a0718cef57f5e3385afd670424999997db8e3d
jiade dai <huolingdfsq@gmail.com>
2020-02-14 12:21:01 +0800

    Update App.vue

--- Frontend/vue2ts/src/App.vue
+++ Frontend/vue2ts/src/App.vue
@@ -1,28 +1,10 @@
 <template>
   <div id="app">
     <router-view />
-    <p>use v-lazy-container work with raw HTML</p>
-    <div v-lazy-container="{ selector: 'img' }">
-      <img data-src="//domain.com/img1.jpg" />
-      <img data-src="//domain.com/img2.jpg" />
-      <img data-src="//domain.com/img3.jpg" />
-    </div>
-    <p>custom error and loading placeholder image</p>
-    <div v-lazy-container="{ selector: 'img', error: 'xxx.jpg', loading: 'xxx.jpg' }">
-      <img data-src="//domain.com/img1.jpg" />
-      <img data-src="//domain.com/img2.jpg" />
-      <img data-src="//domain.com/img3.jpg" />
-    </div>
-    <div v-lazy-container="{ selector: 'img' }">
-      <img data-src="//domain.com/img1.jpg" data-error="xxx.jpg" />
-      <img data-src="//domain.com/img2.jpg" data-loading="xxx.jpg" />
-      <img data-src="//domain.com/img3.jpg" />
-    </div>
   </div>
 </template>
-
 <script lang="ts">
-import { Component, Prop, PropSync, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class App extends Vue {}
