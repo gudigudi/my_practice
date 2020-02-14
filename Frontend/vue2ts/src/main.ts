f62bbd22a900acfcc74f6f25ee6ac4d1f8c604ab
jiade dai <huolingdfsq@gmail.com>
2020-02-14 12:19:50 +0800

    Frontend/vue2ts: remove vue-lazyload.

--- Frontend/vue2ts/src/main.ts
+++ Frontend/vue2ts/src/main.ts
@@ -6,18 +6,10 @@ import i18n from '@/i18n';
 import '@/plugins/composition_api';
 import axios from '@/utils/https';
 import urls from '@/utils/urls';
-import VueLazyload from 'vue-lazyload';
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+import 'ant-design-vue/dist/antd.min.css';
 
 Vue.use(Antd);
-
-Vue.use(VueLazyload, {
-  preLoad: 1.3,
-  error: 'dist/error.png',
-  loading: 'dist/loading.gif',
-  attempt: 1,
-});
 
 Vue.prototype.$https = axios; // 其他页面在使用 axios 的时候直接  this.$http 就可以了
 Vue.prototype.$urls = urls; // 其他页面在使用 urls 的时候直接  this.$urls 就可以了
