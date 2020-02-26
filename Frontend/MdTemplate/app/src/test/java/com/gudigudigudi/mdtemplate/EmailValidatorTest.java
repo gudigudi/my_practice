2fcb27dc2059acd0a1b3dc04198290dd42da6d2c
jiade dai <huolingdfsq@gmail.com>
2020-02-26 14:21:20 +0800

    Update EmailValidatorTest.java

--- Frontend/MdTemplate/app/src/test/java/com/gudigudigudi/mdtemplate/EmailValidatorTest.java
+++ Frontend/MdTemplate/app/src/test/java/com/gudigudigudi/mdtemplate/EmailValidatorTest.java
@@ -1,46 +1,52 @@
 package com.gudigudigudi.mdtemplate;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DisplayName("Unit tests for the EmailValidator logic.")
+@DisplayName("EmailValidator")
 class EmailValidatorTest {
+    @Nested
+    class CorrectEmail {
+        @Test
+        void simple_ReturnsTrue() {
+            assertTrue(EmailValidator.isValidEmail("name@email.com"));
+        }
 
-    @Test
-    void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-        assertTrue(EmailValidator.isValidEmail("name@email.com"));
+        @Test
+        void subDomain_ReturnsTrue() {
+            assertTrue(EmailValidator.isValidEmail("name@email.co.uk"));
+        }
+    }
+
+    @Nested
+    class InvalidEmail {
+        @Test
+        void NoTld_ReturnsFalse() {
+            assertFalse(EmailValidator.isValidEmail("name@email"));
+        }
+
+        @Test
+        void doubleDot_ReturnsFalse() {
+            assertFalse(EmailValidator.isValidEmail("name@email..com"));
+        }
+
+        @Test
+        void noUsername_ReturnsFalse() {
+            assertFalse(EmailValidator.isValidEmail("@email.com"));
+        }
     }
 
     @Test
-    void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
-        assertTrue(EmailValidator.isValidEmail("name@email.co.uk"));
-    }
-
-    @Test
-    void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
-        assertFalse(EmailValidator.isValidEmail("name@email"));
-    }
-
-    @Test
-    void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
-        assertFalse(EmailValidator.isValidEmail("name@email..com"));
-    }
-
-    @Test
-    void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
-        assertFalse(EmailValidator.isValidEmail("@email.com"));
-    }
-
-    @Test
-    void emailValidator_EmptyString_ReturnsFalse() {
+    void emptyString_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(""));
     }
 
     @Test
-    void emailValidator_NullEmail_ReturnsFalse() {
+    void nullEmail_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(null));
     }
 }
