e71071082349b188c3f0dbcb0102af454b3d48aa
jiade dai <huolingdfsq@gmail.com>
2020-03-29 16:32:14 +0800

    Frontend/MdTemplate: remove BDD testcase.

--- Frontend/MdTemplate/app/src/androidTest/java/com/gudigudigudi/mdtemplate/fragment/CucumberTestCase.java
+++
@@ -1,5 +0,0 @@
-package com.gudigudigudi.mdtemplate.fragment;
-
-@CucumberOptions(glue = "com.mytest.steps", format = {"junit:/data/data/com.mytest/JUnitReport.xml", "json:/data/data/com.mytest/JSONReport.json"}, tags = { "~@wip" }, features = "features")
-public class CucumberTestCase {
-}
