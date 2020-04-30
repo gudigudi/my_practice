438ecbf2f85e80546bec9a30345f43f1de638368
jiade dai <huolingdfsq@gmail.com>
2020-04-30 17:51:43 +0800

    refactor(Frontend/MdTemplate/appmodule(demojetpack)): replace AsyncTask code with inline class `QueryTask`

--- Frontend/MdTemplate/appdemojetpack/src/main/java/com/gudigudigudi/appdemojetpack/business/weather/ChooseAreaFragment.java
+++ Frontend/MdTemplate/appdemojetpack/src/main/java/com/gudigudigudi/appdemojetpack/business/weather/ChooseAreaFragment.java
@@ -12,15 +12,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.room.Room;
 
+import com.blankj.utilcode.util.GsonUtils;
 import com.blankj.utilcode.util.ToastUtils;
-import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.gudigudigudi.appdemojetpack.business.weather.room.model.City;
 import com.gudigudigudi.appdemojetpack.business.weather.room.model.County;
 import com.gudigudigudi.appdemojetpack.business.weather.room.model.Province;
 import com.gudigudigudi.appdemojetpack.databinding.FragmentChooseAreaBinding;
 import com.gudigudigudi.commonlib.base.BaseFragment;
-import com.gudigudigudi.commonlib.constants.LogTag;
 import com.gudigudigudi.commonlib.util.HttpUtil;
 
 import java.io.IOException;
@@ -34,11 +33,6 @@ import okhttp3.Response;
 
 @Slf4j
 public class ChooseAreaFragment extends BaseFragment {
-
-    private static final int LEVEL_PROVINCE = 0;
-    private static final int LEVEL_CITY = 1;
-    private static final int LEVEL_COUNTY = 2;
-
     private ArrayAdapter<String> adapter;
     private List<String> dataList = new ArrayList<>();
 
@@ -48,7 +42,13 @@ public class ChooseAreaFragment extends BaseFragment {
 
     private Province currentProvince;
     private City currentCity;
-    private int currentLevel;
+    private QueryLevelCode currentLevelCode;
+
+    private enum QueryLevelCode {
+        CITY,
+        PROVINCE,
+        COUNTRY
+    }
 
     private AppDatabase appDatabase;
 
@@ -73,156 +73,129 @@ public class ChooseAreaFragment extends BaseFragment {
         appDatabase = Room.databaseBuilder(getContext(), AppDatabase.class, "app").build();
 
         binding.listView.setOnItemClickListener((view12, view1, i, l) -> {
-            if (currentLevel == LEVEL_PROVINCE) {
-                currentProvince = provinceList.get(i);
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... voids) {
-                        queryCities();
-                        return null;
+            switch (currentLevelCode) {
+                case CITY:
+                    currentCity = cityList.get(i);
+                    new QueryTask().execute(QueryLevelCode.COUNTRY);
+                case PROVINCE:
+                    currentProvince = provinceList.get(i);
+                    new QueryTask().execute(QueryLevelCode.CITY);
+                case COUNTRY:
+                    String weatherId = countyList.get(i).getWeatherId();
+                    if (getActivity() instanceof ChooseAreaActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.binding.drawerLayout.closeDrawers();
+                        activity.binding.swipeRefleshLayout.setRefreshing(true);
+                        activity.requestWeather(weatherId);
                     }
-                }.execute();
-            } else if (currentLevel == LEVEL_CITY) {
-                currentCity = cityList.get(i);
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... voids) {
-                        queryCounties();
-                        return null;
-                    }
-                }.execute();
-            } else if (currentLevel == LEVEL_COUNTY) {
-                String weatherId = countyList.get(i).getWeatherId();
-                if (getActivity() instanceof ChooseAreaActivity) {
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
-                } else if (getActivity() instanceof WeatherActivity) {
-                    WeatherActivity activity = (WeatherActivity) getActivity();
-                    activity.binding.drawerLayout.closeDrawers();
-                    activity.binding.swipeRefleshLayout.setRefreshing(true);
-                    activity.requestWeather(weatherId);
-                }
             }
         });
 
         binding.btnBack.setOnClickListener(view13 -> {
-            log.debug(LogTag.LOG_VIEW_IS_CLICKED, "button back");
-            if (currentLevel == LEVEL_COUNTY) {
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... voids) {
-                        queryCities();
-                        return null;
-                    }
-                }.execute();
-            } else if (currentLevel == LEVEL_CITY) {
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... voids) {
-                        queryProvinces();
-                        return null;
-                    }
-                }.execute();
-            } else if (currentLevel == LEVEL_PROVINCE) {
-                binding.btnBack.setVisibility(View.GONE);
+            switch (currentLevelCode) {
+                case CITY:
+                    new QueryTask().execute(QueryLevelCode.PROVINCE);
+                case PROVINCE:
+                    binding.btnBack.setVisibility(View.GONE);
+                case COUNTRY:
+                    new QueryTask().execute(QueryLevelCode.CITY);
             }
         });
 
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                queryProvinces();
-                return null;
-            }
-        }.execute();
-
+        new QueryTask().execute(QueryLevelCode.PROVINCE);
         getActivity().runOnUiThread(() -> adapter.notifyDataSetChanged());
-
     }
 
-    private void queryProvinces() {
-        getActivity().runOnUiThread(() -> {
-            binding.txtvTitle.setText("中国");
-            binding.btnBack.setVisibility(View.GONE);
-        });
+    /**
+     * 查询数据：city/province/country
+     */
+    private class QueryTask extends AsyncTask<QueryLevelCode, Void, Void> {
+        @Override
+        protected Void doInBackground(QueryLevelCode... queryLevelCodes) {
+            QueryLevelCode code = queryLevelCodes[0];
+            switch (code) {
+                case CITY:
+                    getActivity().runOnUiThread(() -> {
+                        binding.txtvTitle.setText(currentProvince.getName());
+                        binding.btnBack.setVisibility(View.VISIBLE);
+                    });
 
-        provinceList = appDatabase.provinceDao().getAll();
+                    cityList = appDatabase.cityDao().getCityInProvince(currentProvince.getCode());
 
-        if (!provinceList.isEmpty()) {
-            log.debug("query from local db.");
-            dataList.clear();
+                    if (!cityList.isEmpty()) {
+                        dataList.clear();
 
-            for (Province province : provinceList) {
-                dataList.add(province.getName());
+                        for (City city : cityList) {
+                            dataList.add(city.getName());
+                        }
+                        getActivity().runOnUiThread(() -> {
+                            adapter.notifyDataSetChanged();
+                            binding.listView.setSelection(0);
+                        });
+                        currentLevelCode = code;
+                    } else {
+                        queryFromServer(API_URL + currentProvince.getCode(), code);
+                    }
+                    break;
+                case PROVINCE:
+                    getActivity().runOnUiThread(() -> {
+                        binding.txtvTitle.setText("中国");
+                        binding.btnBack.setVisibility(View.GONE);
+                    });
+
+                    provinceList = appDatabase.provinceDao().getAll();
+
+                    if (!provinceList.isEmpty()) {
+                        dataList.clear();
+
+                        for (Province province : provinceList) {
+                            dataList.add(province.getName());
+                        }
+                        getActivity().runOnUiThread(() -> {
+                            adapter.notifyDataSetChanged();
+                            binding.listView.setSelection(0);
+                        });
+                        currentLevelCode = code;
+                    } else {
+                        queryFromServer(API_URL, code);
+                    }
+                    break;
+                case COUNTRY:
+                    getActivity().runOnUiThread(() -> {
+                        binding.txtvTitle.setText(currentCity.getName());
+                        binding.btnBack.setVisibility(View.VISIBLE);
+                    });
+
+                    countyList = appDatabase.countyDao().getCountyInCity(currentCity.getCode());
+
+                    if (!countyList.isEmpty()) {
+                        dataList.clear();
+                        for (County county : countyList) {
+                            dataList.add(county.getName());
+                        }
+                        getActivity().runOnUiThread(() -> {
+                            adapter.notifyDataSetChanged();
+                            binding.listView.setSelection(0);
+                        });
+                        currentLevelCode = code;
+                    } else {
+                        queryFromServer(API_URL + currentProvince.getCode() + "/" + currentCity.getCode(), code);
+                    }
+                    break;
+                default:
+                    break;
             }
-            getActivity().runOnUiThread(() -> {
-                adapter.notifyDataSetChanged();
-                binding.listView.setSelection(0);
-            });
-            currentLevel = LEVEL_PROVINCE;
-        } else {
-            log.debug("query from server api.");
-            queryFromServer(API_URL, "province");
+            return null;
         }
     }
 
-    private void queryCities() {
-        getActivity().runOnUiThread(() -> {
-            binding.txtvTitle.setText(currentProvince.getName());
-            binding.btnBack.setVisibility(View.VISIBLE);
-        });
-
-        cityList = appDatabase.cityDao().getCityInProvince(currentProvince.getCode());
-
-        if (cityList.size() > 0) {
-            dataList.clear();
-
-            for (City city : cityList) {
-                dataList.add(city.getName());
-            }
-            getActivity().runOnUiThread(() -> {
-                adapter.notifyDataSetChanged();
-                binding.listView.setSelection(0);
-            });
-            currentLevel = LEVEL_CITY;
-        } else {
-            int provinceCode = currentProvince.getCode();
-            String address = API_URL + provinceCode;
-            log.debug(address);
-            queryFromServer(address, "city");
-        }
-    }
-
-    private void queryCounties() {
-        getActivity().runOnUiThread(() -> {
-            binding.txtvTitle.setText(currentCity.getName());
-            binding.btnBack.setVisibility(View.VISIBLE);
-        });
-
-        countyList = appDatabase.countyDao().getCountyInCity(currentCity.getCode());
-
-        if (!countyList.isEmpty()) {
-            dataList.clear();
-            for (County county : countyList) {
-                dataList.add(county.getName());
-            }
-            getActivity().runOnUiThread(() -> {
-                adapter.notifyDataSetChanged();
-                binding.listView.setSelection(0);
-            });
-            currentLevel = LEVEL_COUNTY;
-        } else {
-            int provinceCode = currentProvince.getCode();
-            int cityCode = currentCity.getCode();
-            String address = API_URL + provinceCode + "/" + cityCode;
-            queryFromServer(address, "county");
-        }
-    }
-
-    private void queryFromServer(String address, final String queryLevelCode) {
-        HttpUtil.sendOkHttpRequest(address, new Callback() {
+    private void queryFromServer(String address, QueryLevelCode code) {
+        HttpUtil.sendRequest(address, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 getActivity().runOnUiThread(() -> ToastUtils.showShort("Fail to load"));
@@ -231,51 +204,28 @@ public class ChooseAreaFragment extends BaseFragment {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String responseBody = response.body().string();
-                Gson gson = new Gson();
-                log.debug(queryLevelCode);
 
-                if ("province".equals(queryLevelCode)) {
-                    List<Province> provinces = gson.fromJson(responseBody, new TypeToken<List<Province>>() {
-                    }.getType());
-                    appDatabase.provinceDao().insertAll(provinces);
-                    log.debug("query server api: province");
-                    new AsyncTask<Void, Void, Void>() {
-                        @Override
-                        protected Void doInBackground(Void... voids) {
-                            queryProvinces();
-                            return null;
+                switch (code) {
+                    case CITY:
+                        List<City> cities = GsonUtils.fromJson(responseBody, new TypeToken<List<City>>() {
+                        }.getType());
+                        for (City city : cities) {
+                            city.setProvinceId(currentProvince.getCode());
                         }
-                    }.execute();
-                } else if ("city".equals(queryLevelCode)) {
-                    List<City> cities = gson.fromJson(responseBody, new TypeToken<List<City>>() {
-                    }.getType());
-                    for (City city : cities) {
-                        city.setProvinceId(currentProvince.getCode());
-                    }
-                    appDatabase.cityDao().insertAll(cities);
-                    new AsyncTask<Void, Void, Void>() {
-                        @Override
-                        protected Void doInBackground(Void... voids) {
-                            queryCities();
-                            return null;
+                        appDatabase.cityDao().insertAll(cities);
+                    case PROVINCE:
+                        List<Province> provinces = GsonUtils.fromJson(responseBody, new TypeToken<List<Province>>() {
+                        }.getType());
+                        appDatabase.provinceDao().insertAll(provinces);
+                    case COUNTRY:
+                        List<County> counties = GsonUtils.fromJson(responseBody, new TypeToken<List<County>>() {
+                        }.getType());
+                        for (County county : counties) {
+                            county.setCityId(currentCity.getCode());
                         }
-                    }.execute();
-                } else if ("county".equals(queryLevelCode)) {
-                    List<County> counties = gson.fromJson(responseBody, new TypeToken<List<County>>() {
-                    }.getType());
-                    for (County county : counties) {
-                        county.setCityId(currentCity.getCode());
-                    }
-
-                    appDatabase.countyDao().insertAll(counties);
-                    new AsyncTask<Void, Void, Void>() {
-                        @Override
-                        protected Void doInBackground(Void... voids) {
-                            queryCounties();
-                            return null;
-                        }
-                    }.execute();
+                        appDatabase.countyDao().insertAll(counties);
                 }
+                new QueryTask().execute(code);
             }
         });
     }
